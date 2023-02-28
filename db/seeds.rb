534a09e72afc872d57c0fb31e130b8a59e0cc210
Léa Besse <122352275+Viewfinderr@users.noreply.github.com>
2023-02-28 15:09:16 +0100

    seed is done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+5.times do
+  user = User.create!(first_name: Faker::Movies::HarryPotter.character, last_name: Faker::Movies::HarryPotter.house, email: Faker::Internet.email, password: Faker::Internet.password(min_length: 8))
+end
+
+10.times do
+  beast = Beast.new(race: Faker::Fantasy::Tolkien.race, danger_gauge: 6, tags: Faker::Verb.past_participle, price_per_day: 35)
+  beast.user = User.all.sample
+  beast.save!
+end
