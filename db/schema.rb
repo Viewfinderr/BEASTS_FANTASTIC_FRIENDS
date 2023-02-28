f6b0d162452dc166cda1034a909a6c56bbb16161
Léa Besse <122352275+Viewfinderr@users.noreply.github.com>
2023-02-28 11:39:16 +0100

    controller booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_150006) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_100236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_150006) do
     t.datetime "updated_at", null: false
     t.string "tags", default: [], array: true
     t.index ["user_id"], name: "index_beasts_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "beast_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.float "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["beast_id"], name: "index_bookings_on_beast_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_150006) do
   end
 
   add_foreign_key "beasts", "users"
+  add_foreign_key "bookings", "beasts"
+  add_foreign_key "bookings", "users"
 end
