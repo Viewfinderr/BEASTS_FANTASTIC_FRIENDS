a995ce30158b50c93c520530171f5bb808e2c2c7
Léa Besse <122352275+Viewfinderr@users.noreply.github.com>
2023-03-01 14:30:36 +0100

    ok

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,22 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static values = {
-      apiKey: String,
-      markers: Array
-    }
+    // static values = {
+    //   apiKey: String,
+    //   markers: Array
+    // }
 
-    connect() {
-      mapboxgl.accessToken = this.apiKeyValue
+    // connect() {
+    //   mapboxgl.accessToken = this.apiKeyValue
 
-      this.map = new mapboxgl.Map({
-        container: this.element,
-        style: "mapbox://styles/mapbox/streets-v10"
-      })
+    //   this.map = new mapboxgl.Map({
+    //     container: this.element,
+    //     style: "mapbox://styles/mapbox/streets-v10"
+    //   })
 
-      this.#addMarkersToMap()
-      this.#fitMapToMarkers()
-    }
+    //   this.#addMarkersToMap()
+    //   this.#fitMapToMarkers()
+    // }
 
     #addMarkersToMap() {
       this.markersValue.forEach((marker) => {
