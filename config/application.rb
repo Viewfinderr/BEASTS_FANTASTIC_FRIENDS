acf9aeb5a85869145697935ac64b52b374c66699
Léa Besse <122352275+Viewfinderr@users.noreply.github.com>
2023-03-01 15:40:54 +0100

    push SA MERE

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module BEASTSFantasticFriends
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.css_compressor = nil
+
 
     # Configuration for the application, engines, and railties goes here.
     #
