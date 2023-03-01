701380c9ede565678a396c15f59d258a6795de6b
david rabuteau <david.rabuteau@hotmail.fr>
2023-03-01 11:33:50 +0100

    migration de beast name / description et tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_111009) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_100437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_111009) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "tags", default: [], array: true
+    t.string "name"
+    t.string "description"
     t.index ["user_id"], name: "index_beasts_on_user_id"
   end
 
