5f19861f984cc4fa73caa4dc5252e6b1dd37eb75
david rabuteau <david.rabuteau@hotmail.fr>
2023-03-02 15:31:44 +0100

    srachbarre

--- app/controllers/beasts_controller.rb
+++ app/controllers/beasts_controller.rb
@@ -9,6 +9,11 @@ class BeastsController < ApplicationController
         info_window_html: render_to_string(partial: "info_window", locals: { beast: beast }),
         marker_html: render_to_string(partial: "marker")
       }
+      if params[:query].present?
+        @beasts = Beast.search_by_name_race_tags(params[:query])
+      else
+        @beasts = Beast.all
+      end
     end
   end
 
@@ -46,6 +51,6 @@ class BeastsController < ApplicationController
   private
 
   def beasts_params
-    params.require(:beast).permit(:race, :price_per_day, :danger_gauge, :name, :description, photos: [], tags: [])
+    params.require(:beast).permit(:race, :price_per_day, :danger_gauge, :name, :address, :description, photos: [], tags: [])
   end
 end
